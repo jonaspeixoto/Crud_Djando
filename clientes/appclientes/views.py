16435578e695738cd0a01b1d149bd63131a9ca21
jonas <jonas.silva.087@ufrn.edu.br>
2022-12-08 20:31:50 -0300

    Adicionando listagem por id em clientesw

--- clientes/appclientes/views.py
+++ clientes/appclientes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Cliente
 from .forms import ClientForm
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -19,3 +20,7 @@ def inserir_cliente(request):
         form = ClientForm()
     return render(request, 'clientes/form_clientes.html', {'form': form})
 
+
+def listar_cliente_id(request, id):
+    cliente = Cliente.objects.get(id=id)
+    return render(request, 'clientes/lista_cliente.html', {'cliente': cliente})
