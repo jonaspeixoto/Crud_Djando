00fd03744bb017d30bd98d660d31c95209ad3809
jonas <jonas.silva.087@ufrn.edu.br>
2022-12-15 21:20:47 -0300

    Adicionando função editar cliente

--- clientes/appclientes/views.py
+++ clientes/appclientes/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.template.context_processors import request
+
 from .models import Cliente
 from .forms import ClientForm
 from django.http import HttpResponse
@@ -24,3 +26,12 @@ def inserir_cliente(request):
 def listar_cliente_id(request, id):
     cliente = Cliente.objects.get(id=id)
     return render(request, 'clientes/lista_cliente.html', {'cliente': cliente})
+
+
+def editar_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    form = ClientForm(request.POST or None, instance=cliente)
+    if form.is_valid():
+        form.save()
+        return redirect('listar_clientes')
+    return render(request, 'clientes/form_clientes.html', {'form': form})
